80357e01ca60f4301d6f705400ce47e4b20caed4
serieva <109799187+serieva@users.noreply.github.com>
2024-06-05 20:27:33 -0700

    Update gwas_tools_cli.py

--- linear_gwas/gwas_tools_cli.py
+++ linear_gwas/gwas_tools_cli.py
@@ -1,5 +1,6 @@
 import argparse
 import pandas as pd
+import os
 from .gwas_implem import preprocess_gwas_data, perform_linear_regression
 from .manhattan_plot_and_gwas_results_gen import generate_manhattan_plot_and_gwas_results
 from .qq_plot import generate_qq_plot
@@ -13,15 +14,30 @@ def main():
     parser.add_argument("--out", required = True, help = "Output file prefix")
     
     args = parser.parse_args()
+
+    # Check if user input is correct
+    for file_path in [args.vcf, args.pheno]:
+        if not os.path.isfile(file_path):
+            print(f"Please provide a correct file path. File not found: {file_path}")
+            return
     
     phenotype_file = args.pheno
     genotype_file = args.vcf
     maf_threshold = args.maf
     output_prefix = args.out
 
-    phenotype_data, genotype_data, chrom_dict, pos_dict = preprocess_gwas_data(phenotype_file, genotype_file)
-    
-    results = perform_linear_regression(phenotype_data, genotype_data, chrom_dict, pos_dict)
+    try:
+        phenotype_data, genotype_data, chrom_dict, pos_dict = preprocess_gwas_data(phenotype_file, genotype_file)
+    except Exception as e:
+        print(f"Issue when preprocessing GWAS data: {e}")
+        return
+        
+    try:
+        results = perform_linear_regression(phenotype_data, genotype_data, chrom_dict, pos_dict)
+    except Exception as e:
+        print(f"Issue when performing linear regression: {e}")
+        return
+
     results_df = pd.DataFrame(results, columns = ['CHR', 'SNP', 'BP', 'TEST', 'NMISS', 'BETA', 'STAT', "P"])
     results_df.to_csv(f"{output_prefix}_results.csv", index = False)
     
